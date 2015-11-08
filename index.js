93d3da0f969082548a88a1a2ba65ce802934e6b2
Leonardo Correa <leonardoanalista@gmail.com>
2015-11-09 10:45:12 +1100

    update prompt options

--- index.js
+++ index.js
@@ -1,101 +1,128 @@
-"format cjs";
 
-var wrap = require('word-wrap');
+// Inspired by: https://github.com/commitizen/cz-conventional-changelog/blob/master/index.js
+// https://github.com/commitizen/cz-cli
 
-// This can be any kind of SystemJS compatible module.
-// We use Commonjs here, but ES6 or AMD would do just 
-// fine.
+var CWD = process.cwd();
+// var config = require(CWD + '/cz-config');
+var config = require('./cz-config');
+var wrap = require('./node_modules/word-wrap/index');
+
+
+var types = config.types;
+var scopes = config.scopes;
+var fixScopes = config.fixScopes;
+
+
+function buildCommit(answers) {
+  var maxLineWidth = 100;
+
+  var wrapOptions = {
+    trim: true,
+    newline: '\n',
+    indent:'',
+    width: maxLineWidth
+  };
+
+  function addScope(scope) {
+    if (!scope) return '';
+
+    return '(' + scope.trim() + '): '
+  }
+
+  function addSubject(subject) {
+    if (!subject) return '';
+
+    if(!answers.scope)
+      return ': ' + subject.trim();
+
+    return subject.trim();
+  }
+
+  // Hard limit this line
+  var head = (answers.type + addScope(answers.scope) + addSubject(answers.subject)).slice(0, maxLineWidth);
+
+  // Wrap these lines at 100 characters
+  var body = wrap(answers.body, wrapOptions);
+  body = body.split('|').join('\n');
+
+  var footer = wrap(answers.footer, wrapOptions);
+
+  var result = head
+  if(body) {
+    result += '\n\n' + body;
+  }
+  if(footer) {
+    result += '\n\n' + footer;
+  }
+
+  return result;
+}
+
+var isWip = function(answers) {
+  return answers.type !== 'WIP';
+}
+
 module.exports = {
 
-  // When a user runs `git cz`, prompter will
-  // be executed. We pass you cz, which currently
-  // is just an instance of inquirer.js. Using
-  // this you can ask questions and get answers.
-  // 
-  // The commit callback should be executed when
-  // you're ready to send back a commit template
-  // to git. 
-  // 
-  // By default, we'll de-indent your commit
-  // template and will keep empty lines.
   prompter: function(cz, commit) {
 
     console.log('\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n');
-    
-    // Let's ask some questions of the user
-    // so that we can populate our commit 
-    // template. 
-    //
-    // See inquirer.js docs for specifics.
-    // You can also opt to use another input
-    // collection library if you prefer.
+
     cz.prompt([
       {
         type: 'list',
         name: 'type',
-        message: 'Select the type of change that you\'re committing:',
-        choices: [
-        {
-          name: 'feat:     A new feature',
-          value: 'feat'
-        }, {
-          name: 'fix:      A bug fix',
-          value: 'fix'
-        }, {
-          name: 'docs:     Documentation only changes',
-          value: 'docs'
-        }, {
-          name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
-          value: 'style'
-        }, {
-          name: 'refactor: A code change that neither fixes a bug or adds a feature',
-          value: 'refactor'
-        }, {
-          name: 'perf:     A code change that improves performance',
-          value: 'perf'
-        }, {
-          name: 'test:     Adding missing tests',
-          value: 'test'
-        }, {
-          name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
-          value: 'chore'
-        }]
-      }, {
-        type: 'input',
+        message: '\nSelect the type of change that you\'re committing:',
+        choices: types
+      },
+
+      {
+        type: 'list',
         name: 'scope',
-        message: 'Denote the scope of this change ($location, $browser, $compile, etc.):\n'
-      }, {
+        message: '\nDenote the SCOPE of this change:\n',
+        choices: function(answers) {
+          if (answers.type === 'fix') {
+            return fixScopes;
+          }
+          return scopes;
+        },
+        when: isWip
+      },
+      {
         type: 'input',
         name: 'subject',
-        message: 'Write a short, imperative tense description of the change:\n'
+        message: '\nWrite a SHORT, IMPERATIVE tense description of the change:\n',
+        validate: function(value) {
+          return !!value;
+        }
       }, {
         type: 'input',
         name: 'body',
-        message: 'Provide a longer description of the change:\n'
+        message: '\nProvide a LONGER description of the change (optional). Use "|" to break new line:\n'
       }, {
         type: 'input',
         name: 'footer',
-        message: 'List any breaking changes or issues closed by this change:\n'
+        message: '\nList any BREAKING CHANGES or ISSUES CLOSED by this change (optional):\n',
+        when: isWip
+      },
+      {
+        type: 'confirm',
+        name: 'confirmCommit',
+        message: function(answers) {
+          var SEP = '###--------------------------------------------------------###';
+          console.info(`\n${SEP}\n` + buildCommit(answers) + `\n${SEP}\n`);
+          return 'Are you sure you want to proceed with the commit above?';
+        }
       }
     ], function(answers) {
-      
-      var maxLineWidth = 100;
+      if (!answers.confirmCommit) {
+        console.info('Commit has been canceled.');
+        return;
+      }
 
-      var wrapOptions = {
-        trim: true,
-        newline: '\n',
-        indent:'',
-        width: maxLineWidth
-      };
-
-      // Hard limit this line
-      var head = (answers.type + '(' + answers.scope.trim() + '): ' + answers.subject.trim()).slice(0, maxLineWidth);
-
-      // Wrap these lines at 100 characters
-      var body = wrap(answers.body, wrapOptions);
-      var footer = wrap(answers.footer, wrapOptions);
-
-      commit(head + '\n\n' + body + '\n\n' + footer);
+      var commitStr = buildCommit(answers);
+      // console.info('>>>>>>>>>>>>> DRY RUN <<<<<<<<<<<<<<<<');
+      commit(commitStr);
     });
   }
 }
