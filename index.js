70bb71963ecf1ab4ba09ee8ac89f8b7338365036
Troy McKinnon <troy.t.mckinnon@gmail.com>
2018-11-13 15:17:09 -0600

    feat: add Node v10 support

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ module.exports = {
         temp.open(null, function(err, info) {
           /* istanbul ignore else */
           if (!err) {
-            fs.write(info.fd, buildCommit(answers, config));
+            fs.writeSync(info.fd, buildCommit(answers, config));
             fs.close(info.fd, function(err) {
               editor(info.path, function (code, sig) {
                 if (code === 0) {
