87bfa0b1dd2573f75617a2e9ec008cf9035dc5ec
Leonardo Correa <leonardoanalista@gmail.com>
2022-07-02 21:43:13 +1000

    docs: comment out skip question

--- cz-config-EXAMPLE.js
+++ cz-config-EXAMPLE.js
@@ -61,7 +61,7 @@ module.exports = {
   allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix'],
   // skip any questions you want
-  skipQuestions: ['body'],
+  // skipQuestions: ['scope', 'body'],
 
   // limit subject length
   subjectLimit: 100,
